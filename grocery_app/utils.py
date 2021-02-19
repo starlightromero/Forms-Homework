8905c7a45aefb8d8f97f5764d434aaf179da31b2
Starlight Romero <starlightromero@protonmail.com>
2021-02-18 21:27:30 -0800

    [update] formatting

--- grocery_app/utils.py
+++ grocery_app/utils.py
@@ -1,8 +1,10 @@
 """Utility classes & functions."""
 import enum
 
+
 class FormEnum(enum.Enum):
     """Helper class to make it easier to use enums with forms."""
+
     @classmethod
     def choices(cls):
         return [(choice.name, choice) for choice in cls]
