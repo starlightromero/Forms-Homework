44e7b4633c289749968a17a86b664a7752af6dad
Starlight Romero <starlightromero@protonmail.com>
2021-02-18 21:29:33 -0800

    [update] added flask_login and flask_bcrypt

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from grocery_app.config import Config
 import os
 
@@ -7,11 +9,24 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = os.urandom(24)
 
-db = SQLAlchemy(app)
 
-from grocery_app.routes import main
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "auth.login"
+bcrypt = Bcrypt(app)
+
+from grocery_app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+from grocery_app.auth.routes import auth
+from grocery_app.main.routes import main
 
 app.register_blueprint(main)
+app.register_blueprint(auth)
+
 
 with app.app_context():
     db.create_all()
